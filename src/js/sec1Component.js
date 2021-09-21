ff49bb505871f5d095ecc3cdd5631b145db88c1e
Ashwani Ranjan <ranjan062001@gmail.com>
2021-09-21 12:55:32 +0530

    Sec 1 SlideShow completed

---
+++ src/js/sec1Component.js
@@ -0,0 +1,22 @@
+var mainIndex = 0;
+
+const sec1Change = () => {
+    var slide = document.getElementsByClassName("back_img");
+    if (mainIndex >= slide.length) {
+        console.log(mainIndex);
+        mainIndex = 0;
+    }
+    for (let i = 0; i < slide.length; i++) {
+        if (!slide[i].classList.contains("sec1_img_inactive"))
+            slide[i].classList.add("sec1_img_inactive");
+        slide[i].classList.remove("sec1_img_active");
+    }
+    slide[mainIndex].classList.remove("sec1_img_inactive");
+    slide[mainIndex].classList.add("sec1_img_active");
+    mainIndex++;
+    setTimeout(() => {
+        sec1Change();
+    }, 2000);
+};
+
+sec1Change();
